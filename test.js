5ed65b15ada04a5520157bc24d7279c80d38414b
Vanessa Madrigal <vanessamadrigal@Vanessas-MacBook-Air.local>
2023-03-04 18:27:35 -0800

    update

--- test.js
+++ test.js
@@ -2,5 +2,14 @@ console.log('any type of text');
 
 
 for(let i = 0; i < 10; i++){
-console.log('index;', i)
+console.log('index;', i) //logs the i to the console
+
+/*
+
+
+
+
+*/
 }
+
+console.log("hello");
